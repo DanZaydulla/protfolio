1448bed3d0ce2a769ce90ab7839cf5b90bf644be
DanZaydulla <dancodeecom@gmail.com>
2024-06-30 21:25:46 +0300

    Your commit message here

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:Dancodeecome@gmail.com">
+        <a href="mailto:Dancodeecom@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
